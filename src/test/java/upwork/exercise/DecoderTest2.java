9b3cc9f2ec8231b6536d3a139def486afc254a35
jantolli <joseantollini@gmail.com>
2021-02-24 18:54:44 -0600

    Decoder Exercise First Draft

---
+++ src/test/java/upwork/exercise/DecoderTest2.java
@@ -0,0 +1,23 @@
+package upwork.exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DecoderTest2 {
+
+    @Test
+    public void decode1() {
+        String decoded = main.java.upwork.exercise.Decoder.decode("1, 3, 4, 1");
+        System.out.println(decoded);
+        assertEquals( "31111", decoded, "Values do not match");
+    }
+
+    @Test
+    public void decode2() {
+        String decoded = main.java.upwork.exercise.Decoder.decode("4, 8, 2, 7, 1, 4");
+        System.out.println(decoded);
+        assertEquals("8888774", decoded, "Values do not match");
+    }
+
+}
